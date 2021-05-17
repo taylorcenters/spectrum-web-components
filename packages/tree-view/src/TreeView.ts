ad9a449d750b909e1c002d664c324a92caf34e97
Angel Balcarcel <balcarce@adobe.com>
2021-05-17 07:46:17 -0400

    feat(tree-view): add tree-view pattern with multi-select

---
+++ packages/tree-view/src/TreeView.ts
@@ -0,0 +1,272 @@
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import {
+    CSSResultArray,
+    html,
+    property,
+    PropertyValues,
+    TemplateResult,
+} from '@spectrum-web-components/base';
+import { Focusable, getActiveElement } from '@spectrum-web-components/shared';
+
+import {
+    TreeViewItem,
+    TreeViewItemToggleSelectDetail,
+} from './TreeViewItem.js';
+import { TreeViewHeading } from './TreeViewHeading';
+import treeViewStyles from './tree-view.css.js';
+
+/**
+ * @element sp-tree-view
+ * @slot - The `sp-tree-view-item` elements to manage as a tree.
+ * @attr {Boolean} quiet - A treeview with quiet selection.
+ * @attr {Boolean} standalone - Meant to be used outside of a panel. Items in standalone have rounded corners.
+ * @attr {String} selects - Determines how many items can be marked selected at a time. Can be "single" or "multiple".
+ * @fires change - Announces a change in the `selected` item collection property of the tree view root element.
+ */
+export class TreeView extends Focusable {
+    public static get styles(): CSSResultArray {
+        return [treeViewStyles];
+    }
+
+    @property({ type: Boolean, reflect: true })
+    public standalone = false;
+
+    @property({ type: Boolean, reflect: true })
+    public quiet = false;
+
+    @property({ type: String })
+    public selects: undefined | 'single' | 'multiple';
+
+    @property({ type: Boolean, reflect: true, attribute: 'manage-tab-index' })
+    public manageTabIndex = false;
+
+    private lastSelectedIndex: number = -1;
+
+    constructor() {
+        super();
+        this.addEventListener(
+            'sp-tree-view-item:toggle-select',
+            this.handleItemToggleSelect as EventListener
+        );
+    }
+
+    public get isRoot(): boolean {
+        return this.parentElement?.tagName !== 'SP-TREE-VIEW-ITEM';
+    }
+
+    public get selected(): NodeListOf<TreeViewItem> {
+        return this.querySelectorAll('sp-tree-view-item[selected]');
+    }
+
+    public get focusElement(): TreeViewItem | TreeView {
+        const selected = this.querySelector('[selected]') as TreeViewItem;
+        if (selected && !selected.disabled) {
+            return selected;
+        }
+        const items = [...this.querySelectorAll('sp-tree-view-item')];
+        let index = 0;
+        while (index < items.length && items[index] && items[index]) {
+            index += 1;
+        }
+        if (items[index]) {
+            return items[index] as TreeViewItem | TreeView;
+        }
+        return this;
+    }
+
+    private get items(): NodeListOf<TreeViewItem> {
+        return this.querySelectorAll('sp-tree-view-item');
+    }
+
+    public startListeningToKeyboard(): void {
+        const items = this.items;
+        if (items && !items.length) {
+            return;
+        }
+        this.addEventListener('keydown', this.handleKeydown);
+    }
+
+    public stopListeningToKeyboard(): void {
+        this.removeEventListener('keydown', this.handleKeydown);
+    }
+
+    protected render(): TemplateResult {
+        return html`
+            <slot @slotchange=${this.onSlotChange}></slot>
+        `;
+    }
+
+    protected firstUpdated(changes: PropertyValues): void {
+        super.firstUpdated(changes);
+        this.setAttribute('role', this.isRoot ? 'tree' : 'group');
+        if (this.isRoot) {
+            if (this.selects === 'multiple') {
+                this.setAttribute('aria-multiselectable', 'true');
+            }
+
+            this.addEventListener('focusin', this.startListeningToKeyboard);
+            this.addEventListener('focusout', this.stopListeningToKeyboard);
+        }
+    }
+
+    protected onSlotChange(): void {
+        this.manageTabIndexes();
+
+        const flat = this.querySelector(':scope > sp-tree-view-item[indent]');
+        if (flat) {
+            const firstLevel = this.querySelectorAll(
+                ':scope > sp-tree-view-item:not([indent])'
+            );
+            firstLevel.forEach((child) =>
+                child.setAttribute('aria-level', '1')
+            );
+        }
+    }
+
+    protected updated(changes: PropertyValues): void {
+        super.updated(changes);
+        if (changes.has('manageTabIndex')) {
+            const items = [
+                ...this.querySelectorAll('sp-tree-view-item'),
+            ] as TreeViewItem[];
+            items.map((item) => (item.manageTabIndex = this.manageTabIndex));
+        }
+    }
+
+    private handleKeydown(event: KeyboardEvent): void {
+        const { code } = event;
+        if (code !== 'ArrowDown' && code !== 'ArrowUp') {
+            return;
+        }
+
+        event.preventDefault();
+        const direction = code === 'ArrowDown' ? 1 : -1;
+        const item = this.getItemByOffset(direction);
+        if (item) {
+            item.focus();
+            if (event.shiftKey) item.toggleSelected(event);
+        }
+    }
+
+    private getItemByOffset(direction: number): TreeViewItem | undefined {
+        const items = [...this.items];
+        const focused = items.indexOf(getActiveElement(this) as TreeViewItem);
+        let next = focused;
+        let nextItem = items[next];
+        // cycle through the available items in the directions of the offset to find the next non-disabled item
+        while (
+            nextItem &&
+            (this.isDisabledChild(nextItem) || next === focused)
+        ) {
+            next = (items.length + next + direction) % items.length;
+            nextItem = items[next];
+        }
+        // if there are no non-disabled items, skip the work to focus a child
+        if (!nextItem || this.isDisabledChild(nextItem) || next === focused) {
+            return;
+        } else {
+            return nextItem;
+        }
+    }
+
+    private isDisabledChild(child: TreeViewItem): boolean {
+        if (child.disabled) {
+            return true;
+        }
+        let parent = child.parentElement as
+            | TreeViewItem
+            | TreeView
+            | TreeViewHeading;
+        while (
+            parent instanceof TreeViewHeading ||
+            (parent instanceof TreeView && !parent.isRoot) ||
+            (!(parent as TreeViewItem).disabled &&
+                parent instanceof TreeViewItem &&
+                parent.open)
+        ) {
+            parent = parent.parentElement as
+                | TreeViewItem
+                | TreeView
+                | TreeViewHeading;
+        }
+        return parent !== this;
+    }
+
+    private handleItemToggleSelect(
+        event: CustomEvent<TreeViewItemToggleSelectDetail> & {
+            target: TreeViewItem;
+        }
+    ): void {
+        if (!this.isRoot) return;
+
+        event.stopPropagation();
+        if (this.selects === undefined) {
+            event.preventDefault();
+            return;
+        }
+
+        const { target, detail } = event;
+        if (!target) return;
+
+        const targetIndex = [...this.items].indexOf(target);
+        if (this.selects === 'single' || !detail.multiselect) {
+            this.resetSelected(target);
+        } else if (this.lastSelectedIndex >= 0 && detail.contiguous) {
+            this.updateContiguousItems(targetIndex, target.selected);
+        }
+        if (target.selected) this.lastSelectedIndex = targetIndex;
+        target.updateComplete.then(() => target.focus());
+        this.dispatchEvent(
+            new CustomEvent('change', {
+                bubbles: true,
+                composed: true,
+                cancelable: false,
+            })
+        );
+    }
+
+    private resetSelected(target: TreeViewItem): void {
+        this.selected.forEach((item) => {
+            if (item !== target) item.selected = false;
+        });
+    }
+
+    private updateContiguousItems(
+        targetIndex: number,
+        selected: boolean
+    ): void {
+        const start = Math.min(this.lastSelectedIndex, targetIndex);
+        const end = Math.max(this.lastSelectedIndex, targetIndex);
+        const items = [...this.items].slice(start, end + 1);
+        items.forEach((item) => {
+            if (!item.disabled) item.selected = selected;
+        });
+    }
+
+    private async manageTabIndexes(): Promise<void> {
+        if (this.manageTabIndex && this.selected.length === 0) {
+            const managed = this.querySelector(
+                'sp-tree-view-item:not([tabindex])'
+            ) as TreeViewItem;
+            if (managed) managed.tabIndex = -1;
+            const first = this.querySelector(
+                'sp-tree-view-item:not([disabled])'
+            ) as TreeViewItem;
+            if (first) {
+                await first.updateComplete;
+                first.tabIndex = 0;
+            }
+        }
+    }
+}
---
+++ packages/tree-view/src/TreeViewItem.ts
@@ -0,0 +1,294 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import {
+    html,
+    CSSResultArray,
+    TemplateResult,
+    property,
+    PropertyValues,
+    query,
+    queryAssignedNodes,
+    ifDefined,
+} from '@spectrum-web-components/base';
+
+import {
+    Focusable,
+    ObserveSlotPresence,
+} from '@spectrum-web-components/shared';
+import type { Thumbnail } from '@spectrum-web-components/thumbnail';
+import '@spectrum-web-components/icons-ui/icons/sp-icon-chevron100.js';
+import chevronStyles from '@spectrum-web-components/icon/src/spectrum-icon-chevron.css.js';
+import treeViewItemStyles from './tree-view-item.css.js';
+import treeViewItemLabelStyles from './tree-view-label.css.js';
+import { TreeView } from './TreeView';
+
+export interface TreeViewItemToggleSelectDetail {
+    multiselect: boolean;
+    contiguous: boolean;
+}
+
+/**
+ * @element sp-tree-view-item
+ * @slot icon - The icon that appears on the left of the label
+ * @slot - The label
+ * @attr {Boolean} disabled - Disable an item by preventing selection and focus.
+ * @attr {Boolean} drop-target - Adds styling around an item to identify it as a dropzone.
+ * @attr {Boolean} can-open - In a flat tree, this attribute can be used to identify hierarchical, parent tree-items.
+ * @attr {Boolean} open - Open a parent item that contains children.
+ * @attr {Number} indent - In a flat tree, this attribute can be used to create a hierarchical, nested structure of items.
+ * @attr {Boolean} selected - Mark an item as selected.
+ * @fires change - Announces a change in the `selected` property of the tree view item element.
+ * @fires open - Announces that the tree view item element has been opened.
+ * @fires close - Announces that the tree view item element has been closed.
+ */
+export class TreeViewItem extends ObserveSlotPresence(
+    Focusable,
+    '[slot="thumbnail"]'
+) {
+    public static get styles(): CSSResultArray {
+        return [treeViewItemStyles, treeViewItemLabelStyles, chevronStyles];
+    }
+
+    @property({ type: Boolean, reflect: true })
+    public disabled = false;
+
+    @property({ type: Boolean, reflect: true, attribute: 'drop-target' })
+    public dropTarget = false;
+
+    @query('a')
+    public anchorElement!: HTMLAnchorElement;
+
+    @property({ type: Boolean, reflect: true, attribute: 'can-open' })
+    public canOpen = false;
+
+    @property({ type: Number, reflect: true })
+    public indent!: number;
+
+    @property({ type: Boolean, reflect: true })
+    public open = false;
+
+    @property({ type: Boolean, reflect: true })
+    public selected = false;
+
+    @property({ type: Boolean, attribute: false })
+    public manageTabIndex = false;
+
+    @queryAssignedNodes('thumbnail', true)
+    private assignedThumbnails!: NodeListOf<Thumbnail>;
+
+    private _rootTreeView?: TreeView;
+
+    constructor() {
+        super();
+        this.addEventListener('keydown', this.onKeyDown);
+    }
+
+    public get focusElement(): HTMLAnchorElement {
+        return this.anchorElement;
+    }
+
+    public get thumbnail(): Thumbnail | undefined {
+        if (this.assignedThumbnails.length) {
+            return this.assignedThumbnails[0];
+        }
+
+        return undefined;
+    }
+
+    protected get hasThumbnail(): boolean {
+        return this.slotContentIsPresent;
+    }
+
+    protected get rootTreeView(): TreeView | undefined {
+        if (!this._rootTreeView) {
+            let parent = this.parentElement;
+            if (!parent) return;
+
+            while (!(parent instanceof TreeView && parent.isRoot)) {
+                parent = parent.parentElement;
+                if (parent === null) break;
+            }
+            this._rootTreeView = parent || undefined;
+        }
+        return this._rootTreeView;
+    }
+
+    private get hasChildren(): boolean {
+        return !!this.querySelector('[slot="children"]');
+    }
+
+    public toggleOpen(event: Event): void {
+        event.preventDefault();
+        event.stopPropagation();
+        this.open = !this.open;
+        const applyDefault = this.dispatchEvent(
+            new Event(this.open ? 'open' : 'close', {
+                bubbles: true,
+                composed: true,
+                cancelable: true,
+            })
+        );
+        if (!applyDefault) this.open = !this.open;
+    }
+
+    public toggleSelected(event: MouseEvent | KeyboardEvent): void {
+        event.preventDefault();
+        event.stopPropagation();
+        const multiselect = event.shiftKey || event.metaKey;
+        const contiguous = event.shiftKey;
+        this.selected = !this.selected;
+        const applyDefault = this.dispatchEvent(
+            new CustomEvent<TreeViewItemToggleSelectDetail>(
+                'sp-tree-view-item:toggle-select',
+                {
+                    bubbles: true,
+                    composed: true,
+                    cancelable: true,
+                    detail: { multiselect, contiguous },
+                }
+            )
+        );
+        if (!applyDefault) this.selected = !this.selected;
+    }
+
+    public connectedCallback(): void {
+        super.connectedCallback();
+        this.startManagingTabIndexes();
+    }
+
+    protected render(): TemplateResult {
+        return html`
+            <a
+                href="#"
+                @click=${this.onClick}
+                class=${this.hasThumbnail ? 'has-thumbnail link' : 'link'}
+                aria-hidden=${ifDefined(this.disabled ? 'true' : undefined)}
+            >
+                ${this.hasChildren || this.canOpen
+                    ? html`
+                          <sp-icon-chevron100
+                              id="indicator"
+                              class="spectrum-UIIcon-ChevronRight100"
+                              @click=${this.toggleOpen}
+                          ></sp-icon-chevron100>
+                      `
+                    : html``}
+                <slot name="icon"></slot>
+                <slot name="thumbnail"></slot>
+                <span id="label">
+                    <slot></slot>
+                </span>
+            </a>
+            ${this.open
+                ? html`
+                      <slot name="children"></slot>
+                  `
+                : html``}
+        `;
+    }
+
+    protected firstUpdated(changed: PropertyValues): void {
+        super.firstUpdated(changed);
+        this.setAttribute('role', 'treeitem');
+        if (this.indent && this.indent > 0) {
+            this.setAttribute('aria-level', (this.indent + 1).toString());
+        }
+    }
+
+    protected updated(changed: PropertyValues): void {
+        super.updated(changed);
+        if (this.hasChildren || this.canOpen) {
+            this.setAttribute('aria-expanded', this.open ? 'true' : 'false');
+        }
+        if (!this.disabled) {
+            this.setAttribute(
+                'aria-selected',
+                this.selected ? 'true' : 'false'
+            );
+        }
+        if (changed.has('selected')) {
+            if (this.thumbnail) this.thumbnail.selected = this.selected;
+            if ('undefined' !== typeof changed.get('selected')) {
+                this.dispatchEvent(
+                    new Event('change', {
+                        cancelable: false,
+                    })
+                );
+            }
+        }
+
+        if (changed.has('selected') || changed.has('manageTabIndex')) {
+            const tabIndexForSelectedState = this.selected ? 0 : -1;
+            this.tabIndex = this.manageTabIndex ? tabIndexForSelectedState : 0;
+        }
+    }
+
+    private async startManagingTabIndexes(): Promise<void> {
+        const rootTreeView = this.rootTreeView;
+        if (rootTreeView) {
+            await rootTreeView.updateComplete;
+            this.manageTabIndex = rootTreeView.manageTabIndex;
+        }
+    }
+
+    private onClick(event: MouseEvent): void {
+        if (this.disabled) return;
+
+        this.toggleSelected(event);
+    }
+
+    private onKeyDown(event: KeyboardEvent): void {
+        if (this.disabled) return;
+
+        switch (event.code) {
+            case 'Enter':
+            case 'Space':
+                this.toggleSelected(event);
+                break;
+            case 'ArrowLeft':
+                this.open ? this.toggleOpen(event) : this.focusParent(event);
+                break;
+            case 'ArrowRight':
+                if (!this.hasChildren) return;
+
+                this.open ? this.focusChild(event) : this.toggleOpen(event);
+                break;
+            default:
+                return;
+        }
+    }
+
+    private focusChild(event: Event): void {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const child = this.querySelector(
+            ':scope > sp-tree-view > sp-tree-view-item:not([disabled])'
+        ) as TreeViewItem;
+        if (!child) return;
+
+        child.focus();
+    }
+
+    private focusParent(event: Event): void {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const parent = this.parentElement?.closest(
+            'sp-tree-view-item:not([disabled])'
+        ) as TreeViewItem;
+        if (!parent) return;
+
+        parent.focus();
+    }
+}
